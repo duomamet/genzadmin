e5480158db2611421c1db158e8a4bfca21c84245
duomamet <DUOMAMET@GMAIL.COM>
2025-06-20 10:05:18 +0700

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-firebase.auth().onAuthStateChanged((user) => {
-  if (!user) {
-    if (window.location.pathname !== '/login.html' && window.location.pathname !== '/index.html') {
-      window.location.href = 'login.html';
-    }
-  }
-});
